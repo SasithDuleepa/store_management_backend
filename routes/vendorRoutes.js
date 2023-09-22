ad4c3473e463ddea1743a684ca01ec0c9ee3b51b
SasithDuleepa <duleepajayathissa@gmail.com>
2023-09-22 09:57:20 +0530

    vendor add

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -5,6 +5,6 @@ const AddVenders = require('./../routes_functions/vendor_func/add_vendor');
 const router = express.Router();
 
 
-router.post('/',AddVenders);
+router.post('/add',AddVenders);
 
 module.exports = router;
--- routes_functions/vendor_func/add_vendor.js
+++ routes_functions/vendor_func/add_vendor.js
@@ -1,5 +1,29 @@
+const DB = require('../../config/database');
 
+const AddVendor = (req, res) => {
+    const{Name,address,email,ContactNo,nic}=req.body;
+    if(Name==='' || address==='' || email==='' || ContactNo==='' || nic===''){
+        ressend({status:400, message: 'Please fill all the fields'});
+    }else{
+        const query = `INSERT INTO vendor (vendor_name,vendor_address,vendor_email,vendor_contact_no,vendor_nic) VALUES ('${Name}','${address}','${email}','${ContactNo}','${nic}')`;
+        DB.connection.query(query, (err, result) => {
+            if(result){
+                // res.status(200).send(result);
+                console.log(result)
+                res.send({status:200, message: 'Vendor Added'});
+            }else{
+                console.log(err);
+                res.send({status:500, message: 'internal Error'});
+            }
+        }
+        );
+        
+    }
+    
+}
+    
 
-const AddVendor = () => {}
+    
+
 
 module.exports = AddVendor;
